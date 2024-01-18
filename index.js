1281283e3f65e118f29228cce17586389ee91271
rihab <rihabsalmi2609@gmail.com>
2024-01-18 18:03:14 +0100

    added comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//import the needed npm packages 
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
@@ -12,15 +12,16 @@ inquirer
   ])
   .then((answers) => {
     const url = answers.URL;
-   
+   //generate the image of the link entered by the user with the extension "png"
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
-  
+   //write the link entered by the user into the file named "URL.txt"
     fs.writeFile("URL.txt", url, (err) => {
       if (err) throw err;
       console.log("The file has been saved!");
     });
   })
+  //errors handling
   .catch((error) => {
     if (error.isTtyError) {
       console.log("something went wrong !");
